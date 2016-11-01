803e9046071b17c6755b7aeb38d2165e0bee7613
799782239 <799782239@qq.com>
2016-11-01 15:19:53 +0800

    构建基础代码

--- app/src/main/java/com/jx3/yanqijs/jx3equipment/model/BaseEquipmentModel.java
+++ app/src/main/java/com/jx3/yanqijs/jx3equipment/model/BaseEquipmentModel.java
@@ -8,23 +8,10 @@ public class BaseEquipmentModel {
     private int pId;//装备唯一id
     private String EquipmentName;//装备名称
     private String part;//装备部位
-    private double money;//装备价格
     private double qualityRank;//品质等级
     private double equipmentScore;//装备分数
     private String school;//职业
-
-    private int outDefense;//外功防御
-    private int innerDefense;//内功防御
-    private int constitution;//体质
-    private int attribute;//职业特殊属性
-    private int attack;//攻击
-    private int special;//会心
-    private int specialEffect;//会心效果BaseRecyclerAdapter
-    private int broken;//破防
-    private int hit;//命中
-    private int wushuang;//无双
-    private int resistSpecial;//御劲
-    private int resistAttack;//化劲
+    private String stand;//突出的属性
 
 
 }
---
+++ app/src/main/java/com/jx3/yanqijs/jx3equipment/model/generalEquipmentModel.java
@@ -0,0 +1,21 @@
+package com.jx3.yanqijs.jx3equipment.model;
+
+/**
+ * Created by yanqijs on 2016/11/1.
+ */
+
+public class GeneralEquipmentModel extends BaseEquipmentModel {
+    private double money;//装备价格
+    private int outDefense;//外功防御
+    private int innerDefense;//内功防御
+    private int constitution;//体质
+    private int attribute;//职业特殊属性
+    private int attack;//攻击
+    private int special;//会心
+    private int specialEffect;//会心效果BaseRecyclerAdapter
+    private int broken;//破防
+    private int hit;//命中
+    private int wushuang;//无双
+    private int resistSpecial;//御劲
+    private int resistAttack;//化劲
+}
